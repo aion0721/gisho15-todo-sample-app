2a1433af0346d9736fe68c5d582e52c9ebb87623
aion0721 <rudolf.aion@gmail.com>
2023-11-04 16:22:30 +0900

    create input box

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
   return (
     <>
-      <h1>Vite + React</h1>
-      Hello, World!
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        placeholder="input your task..."
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      ></input>
     </>
   );
 }
